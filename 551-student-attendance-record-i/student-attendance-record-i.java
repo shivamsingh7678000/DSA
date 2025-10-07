f81ff0512eeed334bbe9492283593f3dd785287a
shivamsingh7678000 <shivam.23scse1420041@galgotiasuniversity.ac.in>
2025-10-07 23:33:46 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (49.74%) - LeetSync

---
+++ 551-student-attendance-record-i/student-attendance-record-i.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean checkRecord(String s) {
+        int absent=0, late=0;
+        for(char ch:s.toCharArray()){
+            if(ch=='A')
+            {
+                absent++;
+                if(absent>=2)
+                return false;
+            }
+            if(ch=='L'){
+                late++;
+                if(late>=3)
+                return false;
+            }
+            else
+                late=0;
+        }
+        return true;
+    }
+}
