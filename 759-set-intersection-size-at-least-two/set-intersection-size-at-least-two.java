5792c1fbf622ecbdc7bda4f8cae830b64cd456e0
shivamsingh7678000 <shivam.23scse1420041@galgotiasuniversity.ac.in>
2025-11-20 21:32:03 +0530

    Time: 17 ms (24.49%) | Memory: 47.6 MB (11.56%) - LeetSync

---
+++ 759-set-intersection-size-at-least-two/set-intersection-size-at-least-two.java
@@ -0,0 +1,28 @@
+class Solution {
+  public int intersectionSizeTwo(int[][] intervals) {
+    int ans = 0;
+    int mx = -1;
+    int secondMax = -1;
+
+    Arrays.sort(intervals, Comparator.comparingInt((int[] interval) -> interval[1])
+                               .thenComparingInt((int[] interval) -> - interval[0]));
+
+    for (int[] interval : intervals) {
+      final int start = interval[0];
+      final int end = interval[1];
+      if (mx >= start && secondMax >= start)
+        continue;
+      if (mx >= start) {
+        secondMax = mx;
+        mx = end;
+        ans += 1;
+      } else {
+        mx = end;            
+        secondMax = end - 1; 
+        ans += 2;
+      }
+    }
+
+    return ans;
+  }
+}
