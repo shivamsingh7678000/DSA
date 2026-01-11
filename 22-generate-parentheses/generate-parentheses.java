8dd2b1b1a1af7b9dbf791b753fd53d45ad414969
shivamsingh7678000 <shivam.23scse1420041@galgotiasuniversity.ac.in>
2026-01-11 16:17:49 +0530

    Time: 1 ms (83.79%) | Memory: 44.2 MB (87.11%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,29 +1,25 @@
 class Solution {
-    public List<String> generateParenthesis(int n) {
-        int openB=0, closeB=0;
-        List<String>ans=new ArrayList<>();
+  public List<String> generateParenthesis(int n) {
+    List<String> ans = new ArrayList<>();
+    dfs(n, n, new StringBuilder(), ans);
+    return ans;
+  }
 
-        helper(n,openB,closeB,"",ans);
-        return ans;
-    }    
-
-    private void helper(int n,int open,int close,String s,List<String>ans){
-        if(open+close==2*n)
-        {
-            ans.add(s);
-            return;
-
-        }
-        if(open<n)
-        {
-            helper(n,open+1,close,s+"(",ans);
-
-        }
-        if(close<open)
-        {
-            helper(n,open,close+1,s+")",ans);
-        }
-        
-        
+  private void dfs(int l, int r, StringBuilder sb, List<String> ans) {
+    if (l == 0 && r == 0) {
+      ans.add(sb.toString());
+      return;
     }
+
+    if (l > 0) {
+      sb.append("(");
+      dfs(l - 1, r, sb, ans);
+      sb.deleteCharAt(sb.length() - 1);
+    }
+    if (l < r) {
+      sb.append(")");
+      dfs(l, r - 1, sb, ans);
+      sb.deleteCharAt(sb.length() - 1);
+    }
+  }
 }
