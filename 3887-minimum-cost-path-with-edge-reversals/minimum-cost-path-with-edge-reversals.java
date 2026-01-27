c80b1162119ef10574d9f53c0f3d8025da2b4657
shivamsingh7678000 <shivam.23scse1420041@galgotiasuniversity.ac.in>
2026-01-27 13:21:57 +0530

    Time: 142 ms (38.87%) | Memory: 267.4 MB (64.78%) - LeetSync

--- 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.java
+++ 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.java
@@ -1,41 +1,32 @@
 class Solution {
+    public int minCost(int n, int[][] edges) {
+        ArrayList<int[]> graph[]=new ArrayList[n];
+        for(int i=0;i<graph.length;i++){
+            graph[i]=new ArrayList<>();
+        }
+        for(int i=0;i<edges.length;i++){
+            graph[edges[i][0]].add(new int[]{edges[i][1],edges[i][2]});
+            graph[edges[i][1]].add(new int[]{edges[i][0],edges[i][2]*2});
+        }
+        int cost[]=new int[n];
+        Arrays.fill(cost,Integer.MAX_VALUE);
+        cost[0]=0;
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->a[1]-b[1]);
+        pq.offer(new int[]{0,0});
+        while(!pq.isEmpty()){
+            int []curr=pq.poll();
 
-  private static final int INF = Integer.MAX_VALUE / 2;
+            for(int i=0;i<graph[curr[0]].size();i++){
+                int nextNode=graph[curr[0]].get(i)[0];
+                int weight=graph[curr[0]].get(i)[1];
+                int dist=cost[curr[0]]+weight;
+                if(dist<cost[nextNode]){
+                    cost[nextNode]=dist;
+                    pq.offer(new int[]{nextNode,dist});
+                }
+            }
+        }
 
-  public int minCost(int n, int[][] edges) {
-      List<List<int[]>> graph = new ArrayList<>();
-      for (int i = 0; i < n; i++) {
-          graph.add(new ArrayList<>());
-      }
-      for (int[] e : edges) {
-          int u = e[0], v = e[1], w = e[2];
-          graph.get(u).add(new int[]{v, w});
-          graph.get(v).add(new int[]{u, w * 2});
-      }
-      int[] dist = new int[n];
-      Arrays.fill(dist, INF);
-      dist[0] = 0;
-      PriorityQueue<int[]> pq = new PriorityQueue<>(
-          (a, b) -> Integer.compare(a[0], b[0])
-      );
-
-      pq.offer(new int[]{0, 0}); 
-
-      while (!pq.isEmpty()) {
-          int[] cur = pq.poll();
-          int d = cur[0], node = cur[1];
-
-          if (d > dist[node]) continue;
-
-          for (int[] edge : graph.get(node)) {
-              int next = edge[0], cost = edge[1];
-              if (dist[next] > d + cost) {
-                  dist[next] = d + cost;
-                  pq.offer(new int[]{dist[next], next});
-              }
-          }
-      }
-
-      return dist[n - 1] >= INF ? -1 : dist[n - 1];
-  }
+        return cost[n-1]==Integer.MAX_VALUE?-1:cost[n-1];
+    }
 }
