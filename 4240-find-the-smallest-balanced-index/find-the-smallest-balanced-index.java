11042e45a4ed6a0dc7490afa8a553e82e69258d9
shivamsingh7678000 <shivam.23scse1420041@galgotiasuniversity.ac.in>
2026-03-09 10:02:07 +0530

    Time: 2 ms (100.00%) | Memory: 143.2 MB (100.00%) - LeetSync

--- 4240-find-the-smallest-balanced-index/find-the-smallest-balanced-index.java
+++ 4240-find-the-smallest-balanced-index/find-the-smallest-balanced-index.java
@@ -1,16 +1,16 @@
 class Solution {
     public int smallestBalancedIndex(int[] nums) {
-        long l=0, r=1;
-        int j=nums.length-1;
-        for(int i=0;i<nums.length;i++){
-            while(j>i && l>=r && (l/r)+1>=nums[j]){
-                r*=nums[j--];
-
+        int i=0,j=nums.length-1;
+        long leftSum=0,rightProd=1;
+        while(j!=i){
+            if(leftSum<rightProd){
+                leftSum+=nums[i];
+                i++;
+            }else{
+                rightProd*=nums[j];
+                j--;
             }
-            if(j==i && l==r) return i;
-            l+=nums[i];
         }
-        return -1;
-        
+        return leftSum==rightProd?i:-1;
     }
 }
