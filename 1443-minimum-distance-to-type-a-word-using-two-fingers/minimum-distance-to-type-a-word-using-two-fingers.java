4d3841bc72152fc6872d730375a58925f4ff467a
shivamsingh7678000 <shivam.23scse1420041@galgotiasuniversity.ac.in>
2026-04-12 09:23:31 +0530

    Time: 8 ms (84.62%) | Memory: 43.6 MB (78.46%) - LeetSync

---
+++ 1443-minimum-distance-to-type-a-word-using-two-fingers/minimum-distance-to-type-a-word-using-two-fingers.java
@@ -0,0 +1,38 @@
+class Solution {
+    private int dist(char a, char b) {
+        int x1 = (a - 'A') / 6, y1 = (a - 'A') % 6;
+        int x2 = (b - 'A') / 6, y2 = (b - 'A') % 6;
+        return Math.abs(x1 - x2) + Math.abs(y1 - y2);
+    }
+
+    public int minimumDistance(String word) {
+        int n = word.length();
+        int[] dp = new int[26];
+        int total = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            char a = word.charAt(i);
+            char b = word.charAt(i + 1);
+
+            int d = dist(a, b);
+            total += d;
+
+            int[] newDp = new int[26];
+
+            for (int j = 0; j < 26; j++) {
+                newDp[j] = Math.max(newDp[j], dp[j]);
+                int saving = dp[j] + d - dist((char)(j + 'A'), b);
+                newDp[a - 'A'] = Math.max(newDp[a - 'A'], saving);
+            }
+
+            dp = newDp;
+        }
+
+        int maxSaving = 0;
+        for (int val : dp) {
+            maxSaving = Math.max(maxSaving, val);
+        }
+
+        return total - maxSaving;
+    }
+}
