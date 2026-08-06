e4090f5c439fdc781b006c73aca2dae4c21dbc41
shivamsingh7678000 <shivam.23scse1420041@galgotiasuniversity.ac.in>
2026-08-06 17:30:23 +0530

    Time: 1 ms (100.00%) | Memory: 42.6 MB (50.26%) - LeetSync

---
+++ 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int smallestNumber(int n, int t) {
+        while(true){
+            if(digitSum(n)%t==0)return n;
+            n++;
+        }
+    }
+    public static int digitSum(int n){
+        int sum=1;
+        while(n>0){
+            sum*=(n%10);
+            n/=10;
+        }
+        return sum;
+    }
+}
