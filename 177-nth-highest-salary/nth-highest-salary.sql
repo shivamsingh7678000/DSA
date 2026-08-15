ba4a4c6869a69194254e5e3786bf8db092b13ad8
shivamsingh7678000 <shivam.23scse1420041@galgotiasuniversity.ac.in>
2026-08-15 17:08:26 +0530

    Time: 536 ms (26.53%) | Memory: 0B (100.00%) - LeetSync

---
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -0,0 +1,4 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (SELECT MAX(salary) FROM(SELECT salary,DENSE_RANK() OVER(ORDER BY salary DESC) AS rnk FROM employee)t WHERE rnk=N);
+END
